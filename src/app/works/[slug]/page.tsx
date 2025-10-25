0adbd42652fb7e6f12061dfed8f2c88dafc80574
baghawan <dianbaghawanputera@gmail.com>
2025-10-25 15:10:30 +0700

    feat(works): attributes

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,36 +1,85 @@
 import { Text } from "@/components/ui";
+import { fetcher } from "@/lib/fetcher";
 import { GalleryImage, SeoProps, WorkBase } from "@/types";
 
 interface WorksData extends WorkBase {
-  expertise?: {
+  expertise: {
     id: string;
     documentId: string;
     name: string;
   };
-  tools?: {
+  tools: {
     id: string;
     documentId: string;
     name: string;
   }[];
-  gallery?: GalleryImage[];
-  seo?: SeoProps;
+  gallery: GalleryImage[];
+  seo: SeoProps;
 }
 
-export default function WorkDetails() {
+interface Attributes {
+  name: string;
+  value: string | string[];
+}
+
+export default async function WorkDetails({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const { data } = await fetcher<WorksData>({ endpoint: `/works/${slug}` });
+  const { title, description, year, expertise, tools, gallery } = data;
+
+  const attributes: Attributes[] = [
+    { name: "Year", value: year },
+    { name: "Type", value: expertise.name },
+    { name: "Tools", value: tools.map(({ name }) => name) },
+  ];
+
   return (
     <section className='xl:-mt-20'>
       <div className='flex flex-col xl:flex-row *:xl:flex-1/2'>
-        <div className='p-5 xl:border-l xl:border-solid xl:border-l-neutral-200 xl:dark:border-l-neutral-700 xl:order-2 xl:py-20'>
+        <div className='py-5 px-8 xl:border-l xl:border-solid xl:border-l-neutral-200 xl:dark:border-l-neutral-700 xl:order-2 xl:py-20'>
           <div className='xl:sticky xl:top-24'>
             <Text
               as='h1'
               variant='heading'
             >
-              Work Details
+              {title}
             </Text>
+            <Text
+              as='p'
+              variant='body'
+              className='mt-4 mb-8 text-neutral-400 dark:text-neutral-500'
+            >
+              {description}
+            </Text>
+            <ul>
+              {attributes.map(({ name, value }) => (
+                <li
+                  key={name}
+                  className='flex items-baseline py-2 border-b border-solid border-b-neutral-200 dark:border-b-neutral-700 *:flex-1/2'
+                >
+                  <Text
+                    as='span'
+                    variant='caption'
+                  >
+                    {name}
+                  </Text>
+                  <Text
+                    as='span'
+                    variant='caption'
+                    className='text-neutral-400'
+                  >
+                    {value}
+                  </Text>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-        <div className='bg-neutral-50 h-[1500px] xl:order-1'></div>
+        <div className='bg-neutral-50 dark:bg-neutral-800 h-[1500px] xl:order-1'></div>
       </div>
     </section>
   );
--- src/features/works/WorkList.tsx
+++ src/features/works/WorkList.tsx
@@ -1,4 +1,4 @@
-import { Card, Text } from "@/components/ui";
+import { Anchor, Card, Text } from "@/components/ui";
 import { WorkListProps } from "@/types";
 import Image from "next/image";
 
@@ -6,15 +6,17 @@ export default function WorkList({ data }: { data: WorkListProps[] }) {
   return (
     <>
       <section className='pb-16 container-fluid grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3'>
-        {data.map(({ documentId, title, expertise, gallery }) => {
+        {data.map(({ documentId, title, expertise, slug, gallery }) => {
           const imageSource = gallery.formats?.medium;
 
           return (
-            <div
+            <Anchor
               key={documentId}
-              className='flex gap-4 flex-col'
+              className='flex gap-3 flex-col items-start'
+              href={`/works/${slug}`}
+              variant='muted'
             >
-              <div className='py-2 border-t border-solid border-t-neutral-200 dark:border-t-neutral-700'>
+              <div className='py-2 border-t border-solid border-t-neutral-200 dark:border-t-neutral-700 w-full'>
                 <Text
                   variant='body'
                   as='h2'
@@ -36,7 +38,7 @@ export default function WorkList({ data }: { data: WorkListProps[] }) {
                   height={imageSource?.height}
                 />
               </Card>
-            </div>
+            </Anchor>
           );
         })}
       </section>
